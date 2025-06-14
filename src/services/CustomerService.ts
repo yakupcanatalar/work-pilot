40b50d6357761e501cb506f916067783f4336358
yakupcanatalar <atalaryakupcan@gmail.com>
2025-06-14 21:51:28 +0300

    Removed unused codes.

--- src/services/CustomerService.ts
+++ src/services/CustomerService.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 import CustomerDTO from '../dtos/CustomerDTO';
-import dotenv from "dotenv";
 
-
-dotenv.config(); 
 //const API_URL = "http://localhost:8080/api/v1/";
 const API_URL = "http://137.184.83.58:8080/api/v1/";
 const CUSTOMER_URL = `${API_URL}customer`;
